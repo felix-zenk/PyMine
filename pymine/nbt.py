d4339be6cce2d2d3f4fcd44ba7d7c54a2a161945
Felix <felix.zenk@web.de>
2023-09-27 17:24:02 +0200

    Make TagType

---
+++ pymine/nbt.py
@@ -0,0 +1,15 @@
+from enum import IntEnum
+
+
+class TagType(IntEnum):
+    END = 0
+    BYTE = 1
+    SHORT = 2
+    INT = 3
+    LONG = 4
+    FLOAT = 5
+    DOUBLE = 6
+    BYTE_ARRAY = 7
+    STRING = 8
+    LIST = 9
+    COMPOUND = 10
