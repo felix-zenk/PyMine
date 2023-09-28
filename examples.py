32b9f294c55e43942ce0b03b515af299873d0621
Felix <felix.zenk@web.de>
2023-09-28 07:19:30 +0200

    Add a block id map

--- examples.py
+++ examples.py
@@ -5,12 +5,7 @@ from pathlib import Path
 
 from PIL import Image
 
-from pymine import Position, World, Chunk, Block, BlockIDs, BlockID, EmptyChunk
-from pymine.game import Game
-from pymine.level import Level
-
-atlas = Image.open('terrain-atlas.tga')
-TEXTURE_SIZE = 16
+from pymine import Position, World, Chunk, BlockIDs, Entities, Game, Level, TextureMapper
 
 
 def height_map(world: World, image_path: str):
@@ -40,12 +35,11 @@ def height_map(world: World, image_path: str):
 def top_down_view(world: World, image_path: str):
     """Create a top-down image of the world"""
     logging.info('Creating top-down image of the world')
-    TEXTURE_SIZE = 16
-    img = Image.new(
+    view = Image.new(
         'RGBA',
         (
-            world.chunks_per_direction * Chunk.X_SIZE * TEXTURE_SIZE,
-            world.chunks_per_direction * Chunk.Z_SIZE * TEXTURE_SIZE
+            world.chunks_per_direction * Chunk.X_SIZE * mapper.TEXTURE_SIZE,
+            world.chunks_per_direction * Chunk.Z_SIZE * mapper.TEXTURE_SIZE
         )
     )
 
@@ -54,89 +48,28 @@ def top_down_view(world: World, image_path: str):
         for block in chunk:
             if block.id == BlockIDs.AIR:
                 continue
-            img.paste(
+            view.paste(
                 Image.alpha_composite(
-                    get_texture(world, block),
+                    mapper.top(block),
                     Image.new(
                         'RGBA',
-                        (TEXTURE_SIZE, TEXTURE_SIZE),
+                        (mapper.TEXTURE_SIZE, mapper.TEXTURE_SIZE),
                         (0, 0, 0, Chunk.Y_SIZE - 1 - block.position.y)
                     )
                 ),
                 (
-                    block.position.x * TEXTURE_SIZE,
-                    block.position.z * TEXTURE_SIZE,
-                    block.position.x * TEXTURE_SIZE + TEXTURE_SIZE,
-                    block.position.z * TEXTURE_SIZE + TEXTURE_SIZE
+                    block.position.x * mapper.TEXTURE_SIZE,
+                    block.position.z * mapper.TEXTURE_SIZE,
+                    block.position.x * mapper.TEXTURE_SIZE + mapper.TEXTURE_SIZE,
+                    block.position.z * mapper.TEXTURE_SIZE + mapper.TEXTURE_SIZE
                 )
             )
 
     image_path = Path(image_path)
     image_path.parent.mkdir(exist_ok=True)
     # Rotate image so that up is north
-    img.rotate(-90).save(image_path)
+    view.rotate(-90).save(image_path)
     logging.info('Saved image to %s', image_path)
-
-
-def get_texture(world: World, b: Block):
-    def atlas_coord(x, y):
-        return atlas.crop((TEXTURE_SIZE * y, TEXTURE_SIZE * x, TEXTURE_SIZE * y + TEXTURE_SIZE, TEXTURE_SIZE * x + TEXTURE_SIZE))
-
-    texture = Image.new('RGBA', (TEXTURE_SIZE, TEXTURE_SIZE), (255, 0, 0, 255))
-    if b.is_transparent:
-        if b.position.y > 0:
-            texture.alpha_composite(get_texture(world, world.get_block(Position(
-                x=b.position.x,
-                y=b.position.y - 1,
-                z=b.position.z
-            ))).convert('RGBA'))
-        if b.id == BlockIDs.LEAVES:
-            texture.alpha_composite(atlas_coord(2, 30))
-        if b.id == BlockIDs.TORCH:
-            torch = atlas_coord(3, 18)
-            texture.alpha_composite(torch.crop((6, 6, 7, 7)))
-        return texture
-
-    if b.id == BlockIDs.GRASS_BLOCK:
-        return atlas_coord(0, 3)
-    if b.id == BlockIDs.LOG:
-        return atlas_coord(1, 9)
-    if b.id == BlockIDs.STONE:
-        return atlas_coord(0, 4)
-    if b.id == BlockIDs.SAND:
-        return atlas_coord(0, 14)
-    if b.id == BlockIDs.DIRT:
-        return atlas_coord(0, 21)
-    if b.id in (BlockIDs.STONE_BRICKS, BlockIDs.STONE_SLAB):
-        return atlas_coord(0, 7)
-    if b.id == BlockIDs.MOSS_STONE_MOSSY_COBBLESTONE:
-        return atlas_coord(0, 6)
-    if b.id == BlockIDs.PLANK:
-        if b.meta == 0:
-            return atlas_coord(0, 22)
-        if b.meta == 1:
-            return atlas_coord(0, 23)
-        if b.meta == 2:
-            return atlas_coord(0, 24)
-        if b.meta == 3:
-            return atlas_coord(0, 25)
-    if b.id == BlockIDs.BRICK_BLOCK:
-        return atlas_coord(0, 28)
-    if b.id == BlockIDs.CLAY_BLOCK:
-        return atlas_coord(0, 13)
-    if b.id == BlockIDs.SANDSTONE:
-        return atlas_coord(0, 18)
-    if b.id == BlockIDs.GRAVEL:
-        return atlas_coord(0, 20)
-    if b.id == BlockIDs.NETHER_BRICK:
-        return atlas_coord(4, 12)
-    if b.id == BlockIDs.NETHERRACK:
-        return atlas_coord(4, 11)
-    if b.id == BlockIDs.BEDROCK:
-        return atlas_coord(0, 11)
-    # If not found return a red image
-    # return Image.new('RGBA', (TEXTURE_SIZE, TEXTURE_SIZE), (b.id, b.id, b.id, 255))
-    return texture
 
 
 def create_flat_world(name: str = 'flat_world'):
@@ -158,16 +91,74 @@ def create_flat_world(name: str = 'flat_world'):
                 ])
                 for idx in range((World.MAX_CHUNKS_PER_DIRECTION // 2) ** 2)
             ]
-        )
+        ),
+        entities=Entities([])
     )
     game.save()
 
 
+def block_id_img(number: int):
+    block_img = Image.new('RGBA', (16, 16), (0, 0, 0, 255))
+    digits = [int(d) for d in str(number)]
+    digit_width = digits_map.width // 10
+    for idx, digit in enumerate(digits):
+        digit_img = digits_map.crop((
+            int(digit) * digit_width,
+            0,
+            int(digit) * digit_width + digit_width,
+            digits_map.height
+        ))
+        padding = (block_img.width - len(digits) * digit_width)
+        min_inline_padding = len(digits) - 1
+        side_padding = (padding - min_inline_padding) // 2
+        block_img.paste(digit_img, (
+            side_padding + idx * (digit_width + 1),
+            block_img.height // 2 - digit_img.height // 2
+        ))
+    return block_img
+
+
+def block_id_map(world, image_path):
+    """Create a top-down image of the world"""
+    logging.info('Creating block ID map of the world')
+
+    img = Image.new(
+        'RGBA',
+        (
+            world.chunks_per_direction * Chunk.X_SIZE * mapper.TEXTURE_SIZE,
+            world.chunks_per_direction * Chunk.Z_SIZE * mapper.TEXTURE_SIZE
+        )
+    )
+
+    logging.info('Rendering blocks')
+    for chunk in world:
+        for block in chunk:
+            if block.id == BlockIDs.AIR:
+                continue
+            img.paste(
+                block_id_img(block.id),
+                (
+                    block.position.x * mapper.TEXTURE_SIZE,
+                    block.position.z * mapper.TEXTURE_SIZE,
+                    block.position.x * mapper.TEXTURE_SIZE + mapper.TEXTURE_SIZE,
+                    block.position.z * mapper.TEXTURE_SIZE + mapper.TEXTURE_SIZE
+                )
+            )
+
+    image_path = Path(image_path)
+    image_path.parent.mkdir(exist_ok=True)
+    # Rotate image so that up is north
+    img.rotate(-90).save(image_path)
+    logging.info('Saved image to %s', image_path)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    digits_map = Image.open('placeholders/digits.png')
     game = Game.load('world')
+    mapper = TextureMapper(world=game.world)
 
-    height_map(world=game.world, image_path='images/height_map.png')
+    # height_map(world=game.world, image_path='images/height_map.png')
     top_down_view(world=game.world, image_path='images/top_down_view.png')
-
-    create_flat_world(name='flat_world')
+    # create_flat_world(name='flat_world')
+    # block_id_map(world=game.world, image_path='images/block_id_map.png')
