433c66d171ff012143bb82f782bbce81a76a1db3
Felix <felix.zenk@web.de>
2023-09-30 23:11:38 +0200

    Add NBT

--- pymine/level.py
+++ pymine/level.py
@@ -4,24 +4,33 @@ import logging
 from os import PathLike
 from pathlib import Path
 
+from pymine.nbt import NBT
+
 
 class Level(object):
     def __init__(self, name: str, seed: int = None, **kwargs):
         self.name = name
         self.seed = seed
 
+    @property
+    def file(self) -> Path:
+        return Path(self.name) / 'level.dat'
+
     def __bytes__(self):
         return b''
 
     @classmethod
     def load(cls, path: str | PathLike) -> Level:
-        logging.getLogger(__name__).info('Loading level from %s', path)
+        logging.getLogger(__name__).debug('Loading level from %s', path)
         path = Path(path)
-        logging.getLogger(__name__).info('Level loaded')
+        NBT.from_file()  # todo data
+
+
+        logging.getLogger(__name__).debug('Level loaded')
         return cls(path.name, **dict())
 
-    def save(self, path: str | PathLike):
-        path = Path(path)
+    def save(self, path: str | PathLike = None):
+        path = Path(path) or self.file
         path.parent.mkdir(exist_ok=True)
         path.write_bytes(bytes(self))
 
--- pymine/nbt.py
+++ pymine/nbt.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
 from enum import IntEnum
+from os import PathLike
+from pathlib import Path
 
 
 class TagType(IntEnum):
@@ -13,3 +17,30 @@ class TagType(IntEnum):
     STRING = 8
     LIST = 9
     COMPOUND = 10
+
+
+class NBT:
+    def __init__(self, tag_type: TagType, name: str, payload: bytes = None):
+        self.tag_type = tag_type
+        self.name = name
+        self.entries = self.load(payload) if tag_type in (TagType.LIST, TagType.COMPOUND) else None
+        self.content = payload
+
+    @classmethod
+    def load(cls, data: bytes) -> NBT:
+        nbt_type = TagType(data[0])
+        name_length = int.from_bytes(data[1:3], 'big')
+        name = data[3:3 + name_length].decode()
+        payload = data[3 + name_length:]
+        return cls(nbt_type, name, payload)
+
+    @classmethod
+    def from_file(cls, path: str | PathLike) -> NBT:
+        path = Path(path)
+        content = path.read_bytes()
+        header = content[:8]
+        nbt_version = int.from_bytes(header[:4], 'little')
+        if nbt_version != 3:
+            raise NotImplementedError(f'NBT version {nbt_version} not supported')
+        file_length = int.from_bytes(header[4:], 'little')
+        return cls.load(content[len(header):file_length + len(header)])
